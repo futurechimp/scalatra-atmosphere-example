115a117671a1e9f3d8e031305e4ec37d753707ca
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-12-01 11:26:21 +0000

    Example code.

--- src/main/scala/org/scalatra/example/atmosphere/NotificationsController.scala
+++ src/main/scala/org/scalatra/example/atmosphere/NotificationsController.scala
@@ -28,6 +28,35 @@ class NotificationsController extends ScalatraServlet
     </html>
   }
 
+
+  atmosphere("/the-chat") {
+    new AtmosphereClient {
+      def receive: AtmoReceive = {
+        case Connected =>
+          println("Client %s is connected" format uuid)
+          broadcast(("author" -> "Someone") ~ ("message" -> "joined the room") ~ ("time" -> (new Date().getTime.toString )), Everyone)
+
+        case Disconnected(ClientDisconnected, _) =>
+          broadcast(("author" -> "Someone") ~ ("message" -> "has left the room") ~ ("time" -> (new Date().getTime.toString )), Everyone)
+
+        case Disconnected(ServerDisconnected, _) =>
+          println("Server disconnected the client %s" format uuid)
+        case _: TextMessage =>
+          send(("author" -> "system") ~ ("message" -> "Only json is allowed") ~ ("time" -> (new Date().getTime.toString )))
+
+        case JsonMessage(json) =>
+          println("Got message %s from %s".format((json \ "message").extract[String], (json \ "author").extract[String]))
+          val msg = json merge (("time" -> (new Date().getTime().toString)): JValue)
+          broadcast(msg) // by default a broadcast is to everyone but self
+          //  send(msg) // also send to the sender
+      }
+    }
+  }
+
+  error {
+    case t: Throwable => t.printStackTrace()
+  }
+
   notFound {
     // remove content type in case it was set through an action
     contentType = null
